5bf6ee6166bbca7698a16c68bd4077d79a1850b7
Vineeth krishnan <Vineethkgk18@gmail.com>
2023-04-23 16:24:00 +0530

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,32 @@
 import '../styles/globals.css'
+import Link from 'next/link'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    
+  <div> 
+    <nav className="border-b p-6">
+
+        <p className=" text-16xl font-bold "  >MetaVerse MarketPlace </p>
+
+        
+
+        <div className='mt-4' >
+          <Link href="/" className='mr-6 text-blue-500' >            
+              Home            
+          </Link>
+          <Link href="/" className='mr-6 text-blue-500'>            
+              Sell Digital Asset             
+          </Link>
+
+        </div>
+
+    </nav>
+  
+    <Component {...pageProps} />
+
+  </div>
+  )
 }
 
 export default MyApp
